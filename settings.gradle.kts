e09f128b0344c5443568321d37f82cb1b12fe20c
skjaere <skjaere@proton.me>
2026-02-15 10:22:05 +0100

    ci: fixing build again

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,15 +4,3 @@ plugins {
 
 rootProject.name = "nzb-streamer"
 
-includeBuild("../kotlin-compression-utils") {
-    dependencySubstitution {
-        substitute(module("com.github.skjaere:kotlin-compression-utils")).using(project(":"))
-    }
-}
-includeBuild("../ktor-usenet-client") {
-    dependencySubstitution {
-        substitute(module("com.github.skjaere:ktor-nntp-client")).using(project(":"))
-    }
-}
-includeBuild("../mock-nntp-server/mock-nntp-server")
-includeBuild("../nzb-streamer-utils")
