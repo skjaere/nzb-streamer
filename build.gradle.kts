30d8bdac7880537577ca56c7d438ff9fa6dcbb74
skjaere <skjaere@proton.me>
2026-02-25 09:32:40 +0100

    fix: bumping ktor-nntp-client to 0.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ java {
 }
 
 dependencies {
-    api("com.github.skjaere:ktor-nntp-client:v0.1.2")
+    api("com.github.skjaere:ktor-nntp-client:v0.2.0")
     api("com.github.skjaere:kotlin-compression-utils:v0.3.0")
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.serialization.json)
